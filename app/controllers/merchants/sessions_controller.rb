6e478c0617df5ef3246e5a98f8eb36ecdab739b1
junwen29 <junwen2992@gmail.com>
2015-09-09 23:51:39 +0800

    Update layouts and clean up codes
    remove unnecessary codes in session controller
    clean up _dash layout
    change home nav bar layout

--- app/controllers/merchants/sessions_controller.rb
+++ app/controllers/merchants/sessions_controller.rb
@@ -8,27 +8,26 @@ class Merchants::SessionsController < Devise::SessionsController
     venues_path
   end
 
-# before_filter :configure_sign_in_params, only: [:create]
-# GET /resource/sign_in
-def new
-  @merchant_page = true
-  super
-end
+  # before_filter :configure_sign_in_params, only: [:create]
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
 
-# POST /resource/sign_in
-# def create
-#   super
-# end
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
 
-# DELETE /resource/sign_out
-# def destroy
-#   super
-# end
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
 
-# protected
+  # protected
 
-# You can put the params you want to permit in the empty array.
-# def configure_sign_in_params
-#   devise_parameter_sanitizer.for(:sign_in) << :attribute
-# end
+  # You can put the params you want to permit in the empty array.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.for(:sign_in) << :attribute
+  # end
 end
