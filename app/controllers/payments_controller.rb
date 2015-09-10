4c65a75cae0d938f3c5715a4cf72969fe63d8065
gsm99 <gohserming@gmail.com>
2015-09-10 15:56:51 +0800

    update bundle

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,20 @@
+class PaymentsController < ApplicationController
+
+  def new
+   # @payment = Payment.new
+  end
+
+  def index
+ #   @payments = Payment.all
+  end
+
+  def create
+
+  end
+ # def create
+ #   for database
+ #   @deal = Deal.new(deal_params)
+ # end
+
+
+end
