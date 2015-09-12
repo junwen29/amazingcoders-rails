4d9623daa33916e6efde221dd8390e511c28cd4e
Jack <jack@shopback.com>
2015-09-13 01:50:05 +0800

    Edited form for deals

--- app/admin/deal.rb
+++ app/admin/deal.rb
@@ -20,6 +20,36 @@ ActiveAdmin.register Deal do
     actions
   end
 
+  form do |f|
+    f.inputs "Deal Associations" do
+      f.input :merchant_id
+    end
+
+    f.inputs "Deal Info" do
+      f.input :title
+      f.input :type_of_deal
+      f.input :description
+      f.input :t_c, label: "Terms and Conditions"
+    end
+
+    f.inputs "Deal Schedule" do
+      f.input :start_date, label: "Active Date"
+      f.input :expiry_date, label: "Expiry Date"
+    end
+
+    f.inputs "Deal Redemption" do
+      f.input :num_of_redeems, label: "Number of redeems"
+      f.input :multiple_use, label: "Multiple Use?"
+    end
+
+    f.inputs "Deal Add-ons" do
+      f.input :pushed, label: "Push Notification to Wishlist?"
+      f.input :redeemable, label: "QR Code Redeemable?"
+    end
+
+    f.actions
+  end
+
   # Allow edit
   permit_params :title, :redeemable, :multiple_use, :image, :type_of_deal, :description, :start_date, :expiry_date, :location, :t_c, :pushed,
                 deal_days_attributes: [:id, :mon, :tue, :wed, :thur, :fri, :sat, :sun, :_destroy,
