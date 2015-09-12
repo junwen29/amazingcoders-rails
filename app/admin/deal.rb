e03690200a1c5d92bbc5b32be313ec720cd48470
Jack <jack@shopback.com>
2015-09-12 15:57:20 +0800

    Added scope to deals dashboard

--- app/admin/deal.rb
+++ app/admin/deal.rb
@@ -1,18 +1,22 @@
 ActiveAdmin.register Deal do
+  scope :active
+  scope :waiting
+  scope :expired
 
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
-
+  index do
+    selectable_column
+    column :id
+    column "Title", :title
+    column "Type", :type_of_deal
+    column "Description", :description
+    column "Active Date", :start_date
+    column "Expiry Date", :expiry_date
+    column "Multiple Use", :multiple_use
+    column "QR Code", :redeemable
+    column "Push Notification", :pushed
+    column "Created At", :created_at
+    column "Updated At", :updated_at
+    actions
+  end
 
 end
