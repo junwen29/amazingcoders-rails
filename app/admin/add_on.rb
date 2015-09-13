91cd88cf615c140443872168b34ba507b9a7148a
Jack <jack@shopback.com>
2015-09-13 20:29:00 +0800

    Manage addons from admin

--- app/admin/add_on.rb
+++ app/admin/add_on.rb
@@ -1,5 +1,5 @@
-ActiveAdmin.register Plan do
-  menu :parent => "Premium Services", :priority => 1
+ActiveAdmin.register AddOn do
+  menu :parent => "Premium Services", :priority => 2
 
   # Allow edit
   permit_params :name, :cost, :description
@@ -13,37 +13,23 @@ ActiveAdmin.register Plan do
     column "Cost", :cost do |plan|
       number_to_currency plan.cost
     end
+    column "Plan", :plan_id
     column "Created At", :created_at
     column "Updated At", :updated_at
     actions
   end
 
-  # SHOW
-  show do |f|
-    panel "Plan Information" do
-      attributes_table_for f do
-        row :id
-        row :name
-        row :cost do |plan|
-          number_to_currency plan.cost
-        end
-        row :description
-        row :created_at
-        row :updated_at
-      end
-    end
-  end
-
-
   # EDIT
   form do |f|
     f.semantic_errors
-    f.inputs "Plan Information" do
+    f.inputs "Add On Information" do
+      f.input :plan_id
       f.input :name
       f.input :cost, as: :string, :hint => "No need to specify currency - defaulted to SGD $. Input to 2 decimal places. e.g. 10.00"
       f.input :description
     end
     f.actions
   end
+
 
 end
