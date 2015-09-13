f3e4fb0d42ee9caf327daef7ebb17ec1b1f6c5d3
Jack <jack@shopback.com>
2015-09-13 18:09:17 +0800

    Fix money format on index, edit and show page

--- app/admin/plan.rb
+++ app/admin/plan.rb
@@ -2,18 +2,47 @@ ActiveAdmin.register Plan do
   # Allow edit
   permit_params :name, :cost, :description
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
+  # INDEX
+  index do
+    selectable_column
+    column :id
+    column "Name", :name
+    column "Cost", :cost do |plan|
+      number_to_currency plan.cost
+    end
+    column "Description", :description
+    column "Created At", :created_at
+    column "Updated At", :updated_at
+    actions
+  end
 
+  # SHOW
+
+  show do |f|
+    panel "Plan Information" do
+      attributes_table_for f do
+        row :id
+        row :name
+        row :cost do |plan|
+          number_to_currency plan.cost
+        end
+        row :description
+        row :created_at
+        row :updated_at
+      end
+    end
+  end
+
+
+  # EDIT
+  form do |f|
+    f.semantic_errors
+    f.inputs "Plan Information" do
+      f.input :name
+      f.input :cost, as: :string, :hint => "No need to specify currency - defaulted to SGD $. Input to 2 decimal places. e.g. 10.00"
+      f.input :description
+    end
+    f.actions
+  end
 
 end
