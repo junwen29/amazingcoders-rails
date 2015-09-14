c1a540a81771e25599947211c4cfafbb220de3b0
Jack <jack@shopback.com>
2015-09-14 15:22:42 +0800

    disable edit for payment and edited views on payment

--- app/admin/payment.rb
+++ app/admin/payment.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Payment do
+  actions :all, except: [:edit] # forbid edit to payment information
 
   index do
     selectable_column
@@ -22,5 +23,27 @@ ActiveAdmin.register Payment do
     actions
   end
 
+  # SHOW
+  show do |f|
+    panel "Payment Details" do
+      attributes_table_for f do
+        row :id
+        row :plan do |payment|
+          payment.plan
+        end
+        row :addons do |payment|
+          payment.add_ons.map{|a| a.name }.join(", ").html_safe
+        end
+        row :start_date, label: "Subscription Date"
+        row :expiry_date, label: "Expired Date"
+        row('Total Paid') do |payment|
+          number_to_currency payment.total_cost
+        end
+        row :created_at
+        row :updated_at
+      end
+    end
+    active_admin_comments # Add this line for comment block
+  end
 
 end
