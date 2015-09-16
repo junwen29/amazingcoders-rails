0cbcaf7b5e63050f5735436abaeb627f84e8ca26
Woonyong92 <woonyong92@gmail.com>
2015-09-16 21:03:40 +0800

    Removed location from deal database

--- db/migrate/20150908041922_create_deals.rb
+++ db/migrate/20150908041922_create_deals.rb
@@ -9,7 +9,6 @@ class CreateDeals < ActiveRecord::Migration
       t.string :description
       t.date :start_date
       t.date :expiry_date
-      t.string :location
       t.string :t_c
       t.integer :num_of_redeems
       t.boolean :pushed
--- db/schema.rb
+++ db/schema.rb
@@ -101,7 +101,6 @@ ActiveRecord::Schema.define(version: 20150915142236) do
     t.string   "description"
     t.date     "start_date"
     t.date     "expiry_date"
-    t.string   "location"
     t.string   "t_c"
     t.integer  "num_of_redeems"
     t.boolean  "pushed"
