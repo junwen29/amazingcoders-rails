840566815c9b8fde21d59b45b8aebd85e7322bee
junwen29 <junwen2992@gmail.com>
2015-09-17 10:10:02 +0800

    added new api helper 'exception'

---
+++ lib/burpple/exceptions.rb
@@ -0,0 +1,50 @@
+module Burpple
+  module Exceptions
+    class BurppleError < StandardError
+      attr_accessor :status, :type, :httpcode
+      def initialize(status, type, httpcode, message)
+        super(message)
+        self.status = status
+        self.type   = type
+        self.httpcode = httpcode
+      end
+    end
+
+    ########
+    class AuthError < BurppleError
+      def initialize(message = 'auth error')
+        super(:auth_error, "Auth Error", 401, message)
+      end
+    end
+    class ExistingError < BurppleError
+      def initialize(message = 'exsiting error')
+        super(:existing_error, "Existing Error", 406, message)
+      end
+    end
+    class DuplicateError < BurppleError
+      def initialize(message = 'conflict error')
+        super(:conflict_error, "Conflict Error", 409, message)
+      end
+    end
+    class ConflictError < BurppleError
+      def initialize(message = 'conflict error')
+        super(:conflict_error, "Conflict Error", 409, message)
+      end
+    end
+    class ForbiddenError < BurppleError
+      def initialize(message = 'something wrong please try again')
+        super(:forbidden, "Forbidden", 403, message)
+      end
+    end
+    class BadRequestError < BurppleError
+      def initialize(message = '')
+        super(:bad_request, "Bad Request", 400, message)
+      end
+    end
+    class NotFoundError < BurppleError
+      def initialize(message = '')
+        super(:not_found, "Not Found", 404, message)
+      end
+    end
+  end
+end
