5d48c5d8736d4ded5fe496d18c8042fa1e2a07f4
junwen29 <junwen2992@gmail.com>
2015-09-20 21:46:26 +0800

    created new user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,35 @@
 class User < ActiveRecord::Base
+
+  include User::Json
+  include User::Authentication
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  def skip_confirmation!
-    self.confirmed_at = Time.now
+  # validation rule
+  validates :first_name, :presence => true, :length => {:maximum => 255}
+  validates :last_name, :presence => true, :length => {:maximum => 255}
+  validate :username_valid, if: :username_changed?
+  validates_uniqueness_of :username, allow_blank: true, allow_nil: true, case_sensitive: false, message: "%{value} is already taken", if: :username_changed?
+
+  def username_valid
+    return if username.nil?
+    return errors.add(:username, "can't be blank") if username.blank?
+
+    # quick fix for username space error
+    self.username = self.username.strip
+
+    valid_characters = "[a-zA-Z_\-]"
+    all_numbers = (username =~ Regexp.new('^(?=.*[' + valid_characters + '])')).nil?
+    return errors.add(:username, "can't be all numbers") if all_numbers
+    invalid_characters = (username =~ Regexp.new('^[' + valid_characters + '0-9]+$')).nil?
+    return errors.add(:username, 'can only contain letters, numbers, and underscore') if invalid_characters
   end
+
+
+  # def skip_confirmation!
+  #   self.confirmed_at = Time.now
+  # end
 end
