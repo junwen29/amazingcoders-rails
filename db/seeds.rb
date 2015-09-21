00d8243264e5960f92318225aeb004c4540a3bec
Jack <jack@shopback.com>
2015-09-21 13:42:33 +0800

    Updated wishlist addon description in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 Plan.create(id: '1', name: 'Premium Deals Services', cost: '30', description: 'Allows unlimited creation of deals. Publishing
 of up to 5 active deals')
 
-AddOn.create(id: '1', name: 'Push Notification', cost: '5', description: 'Allows 1 Push Notification per Deal!', addon_type: 'notification', created_at: Time.current, updated_at: Time.current, plan_id: '1')
+AddOn.create(id: '1', name: 'Push Notification', cost: '5', description: 'Allows 1 Push Notification per deal to Burpple users who have wishlisted your venue!', addon_type: 'notification', created_at: Time.current, updated_at: Time.current, plan_id: '1')
 AddOn.create(id: '2', name: 'Deals Statistics', cost: '5', description: 'See demographics of users to target your deals better!', addon_type: 'statistics', created_at: Time.current, updated_at: Time.current, plan_id: '1')
 AddOn.create(id: '3', name: 'Aggregate Trends', cost: '5', description: 'See popular keywords and trends across different deals!', addon_type: 'trends', created_at: Time.current, updated_at: Time.current, plan_id: '1')
 
