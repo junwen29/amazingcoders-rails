810c42ff226b12d522c0487530a872deba870798
Yesha Simaria <yesha.simaria@u.nus.edu>
2015-09-22 13:16:12 +0800

    Error Page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+  rescue_from ActiveRecord::RecordNotFound, :with => :not_found
+  rescue_from AbstractController::ActionNotFound, :with => :not_found
 
   def merchant_id
     current_merchant.id if merchant_signed_in?
@@ -10,6 +12,14 @@ class ApplicationController < ActionController::Base
 
   def set_merchant_id
     @merchant_id = merchant_id
+  end
+
+  def not_found
+    respond_to do |format|
+      format.html {render "#{Rails.root}/public/404", :layout => false, :status => 404 }
+    end
+  rescue ActionController::UnknownFormat
+    head :not_found
   end
 
 end
