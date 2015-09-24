a1b2facc77a71dad25e8d4a303ee7bb3b75b5b1b
junwen29 <junwen2992@gmail.com>
2015-09-24 22:04:56 +0800

    remove all_to_json method

--- app/controllers/api/p1/deals_controller.rb
+++ app/controllers/api/p1/deals_controller.rb
@@ -10,25 +10,9 @@ class Api::P1::DealsController < Api::P1::ApplicationController
   end
 
   def show
-    # all_details = []
     deal = Deal.find(params[:deal_id])
-    # days = DealDay.where(:deal_id => params[:deal_id])
-    # time = []
-    # days.each do |d|
-    #   time << DealTime.where(:deal_day_id => d.id)
-    # end
-    # all_details << deal
-    # all_details << days
-    # all_details << time
     render_jbuilder do |json|
-      deal.all_to_json(json)
-      # json.set! :deal_days do
-      #   json.array!(deal.deal_days, :id, :deal_id, :mon, :tue, :wed, :thur, :fri, :sat, :sun)
-      #   days = DealDay.where(:deal_id => params[:deal_id])
-      #   days.each do |d|
-      #       json.array!(d.deal_times, :deal_day_id, :started_at, :ended_at)
-      #   end
-      # end
+      deal.to_json json
     end
   end
 end
