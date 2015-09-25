7081932b6eee5ba5aef4bdc718e81524224f5369
junwen29 <junwen2992@gmail.com>
2015-09-25 16:40:08 +0800

    fix code, reason wrong fetching method

--- app/controllers/api/p1/deals_controller.rb
+++ app/controllers/api/p1/deals_controller.rb
@@ -17,12 +17,16 @@ class Api::P1::DealsController < Api::P1::ApplicationController
   end
 
   def get_deals_for_venue
-    all_deal_venue = DealVenue.where(:venue_id => params[:id]).select(:deal_id)
-    deals = []
-    all_deal_venue.each do |d|
-      deal=Deal.find(d.deal_id)
-      deals << deal
-    end
+    # all_deal_venue = DealVenue.where(:venue_id => params[:id])#.select(:deal_id)
+    # # deals = []
+    # deals = Array
+    # all_deal_venue.each do |d|
+    #   deal=Deal.find(d.deal_id)
+    #   deals << deal
+    # end
+    venue = Venue.find(params[:id])
+    deals = venue.deals
+
     #active_deals = []
     #deals.each do
     #  active_deal = Deal.where('expiry_date >= ? AND (start_date = ? OR expiry_date = ? OR (start_date < ? AND expiry_date > ?) OR (expiry_date > ? AND active = true))', DateTime.now, DateTime.now, DateTime.now, DateTime.now, DateTime.now, DateTime.now)
