efd0ed8dcb33d93f6367dd63297c4167dc4e932f
Woonyong92 <woonyong92@gmail.com>
2015-09-27 01:21:27 +0800

    Fixed the bug where even when there is no waiting deals, I cannot activate a waiting deal

--- app/services/deal_service.rb
+++ app/services/deal_service.rb
@@ -5,7 +5,7 @@ class DealService
     def num_active_deals (merchant_id, deal)
       all_deals = Deal.where(:merchant_id => merchant_id)
       valid_deals = all_deals.where('expiry_date >= ?', DateTime.now)
-      active_deals = valid_deals.where('start_date = ? OR expiry_date = ? OR (start_date < ? AND expiry_date > ?) OR (expiry_date > ? AND active = true)', DateTime.now, DateTime.now, DateTime.now, DateTime.now, DateTime.now)
+      active_deals = valid_deals.where('expiry_date > ? AND active = true', DateTime.now)
       active_deals.count
     end
 
