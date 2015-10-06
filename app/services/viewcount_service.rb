c33793273eeae23e92623b642512763d91612580
Jack <jack@shopback.com>
2015-10-06 23:43:11 +0800

    Added service methods for redemption and viewcount

--- app/services/viewcount_service.rb
+++ app/services/viewcount_service.rb
@@ -1,6 +1,28 @@
 class ViewcountService
 
   module ClassMethods
+    # TODO: Call these methods on Android to update view count
+    def getViewCount(deal_id)
+      viewcount = Viewcount.where(:deal_id => deal_id)
+      if viewcount.empty?
+        0
+      else
+        viewcount.count
+      end
+    end
+
+    def setViewCount(deal_id, user_id)
+      Viewcount.create(deal_id: deal_id, user_id: user_id)
+    end
+
+    def is_unique(deal_id, user_id)
+      exist = Viewcount.where('deal_id = ? AND user_id = ?', deal_id, user_id)
+      if exist.empty?
+        true
+      else
+        false
+      end
+    end
 
   end
 
