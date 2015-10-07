8f0f8eb082bb6ab23eb25085dad490a51c1ef5d3
gsm99 <gohserming@gmail.com>
2015-10-07 18:06:44 +0800

    show total burps

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -3,6 +3,7 @@ class PointsController < InheritedResources::Base
 
   def index
     @points = MerchantService.get_all_points(merchant_id)
+    @merchant = Merchant.find(merchant_id)
   end
 
   def point_params
