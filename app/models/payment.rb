2227f2eef9da3363c5564191103524ef111bec22
Jack <jack@shopback.com>
2015-10-10 15:05:32 +0800

    Removed destroy dependency between payments and premium service

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,9 +1,9 @@
 class Payment < ActiveRecord::Base
   #has_many :plans
-  has_many :add_on_payments, dependent: :destroy
+  has_many :add_on_payments
   has_many :add_ons, through:  :add_on_payments
 
-  has_many :plan_payments, dependent: :destroy
+  has_many :plan_payments
   has_many :plans, through:  :plan_payments
   has_one :charge
   
--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,6 +1,6 @@
 class Plan < ActiveRecord::Base
   has_many :add_ons, dependent: :destroy
   #belongs_to :payment
-  has_many :plan_payments, dependent: :destroy
+  has_many :plan_payments
   has_many :payments, through: :plan_payments
 end
