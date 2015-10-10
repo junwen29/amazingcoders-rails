c09fd829f4dd6628f1221d238fb19df8eb84b30c
Jack <jack@shopback.com>
2015-10-10 15:20:39 +0800

    Added validation for plan and add ons

--- app/models/add_on.rb
+++ app/models/add_on.rb
@@ -3,4 +3,9 @@ class AddOn < ActiveRecord::Base
   #belongs_to :payment
   has_many :add_on_payments
   has_many :payments, through: :add_on_payments
+
+  validates(:name, presence: true)
+  validates(:cost, presence: true)
+  validates(:description, presence: true)
+  validates(:addon_type, presence: true)
 end
--- app/models/plan.rb
+++ app/models/plan.rb
@@ -3,4 +3,8 @@ class Plan < ActiveRecord::Base
   #belongs_to :payment
   has_many :plan_payments
   has_many :payments, through: :plan_payments
+
+  validates(:name, presence: true)
+  validates(:cost, presence: true)
+  validates(:description, presence: true)
 end
