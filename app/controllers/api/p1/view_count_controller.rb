bbfd4d8b61176d97080be6489ddf9333569f716a
junwen29 <junwen2992@gmail.com>
2015-10-13 23:08:03 +0800

    update deal_analytic object

--- app/controllers/api/p1/view_count_controller.rb
+++ app/controllers/api/p1/view_count_controller.rb
@@ -6,6 +6,11 @@ class Api::P1::ViewCountController <  Api::P1::ApplicationController
     deal_id = params[:deal_id]
     user_id = get_current_user_id
     Viewcount.create!(deal_id: deal_id, user_id: user_id, entry: entry)
+
+    #  update deal_analytic object
+    DealAnalyticService.set_view_count deal_id
+    DealAnalyticService.set_unique_view_count deal_id
+
     head_ok
   end
 end
