7b4f4bd20d7c8aeb6139ac4b53998332136364e3
Woonyong92 <woonyong92@gmail.com>
2015-10-15 01:11:31 +0800

    Cleaned up DealAnalytics controller to only methods required. Also cleaned up DealAnalytics index page to only render stuff required

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -2,20 +2,13 @@ class AnalyticsController < ApplicationController
   before_action :check_has_analytics_access
 
   def index
-    # TODO: Add all analytics methods
-    # Deal Statistics
-    deal_analytics_by_day
-    deal_analytics_by_type_and_redemption
-    deal_statistics
-    deal_statistics_deal_type
-    # Aggregate Trends
-
-    #raise @past_deal_names.inspect
+    # Deal Statistics by Deals
+    deal_statistics_by_deal
   end
 
   def show
-    deal_analytics_by_venue
-    deal_analytics_by_deals_for_venues
+    # Deal Statistics by Venues
+    deal_statistics_by_venue
     render "analytics/venue"
   end
 
@@ -50,64 +43,44 @@ class AnalyticsController < ApplicationController
   end
 
   # Helper methods
-  def deal_statistics
+  def deal_statistics_by_deal
     if check_has_deal_statistics
-      # Show view count and redemption count
-      deal_statistics_count
+      # Show view count and redemption count in highchart by days
+      deal_analytics_by_day
       # Show popular deal type
-      deal_statistics_deal_type
-    end
-  end
-
-  def deal_statistics_count
-    # Toggle to view active deals or include past deals
-    if params[:scope] == "all"
-      @active_deal_names = MerchantService.get_all_active_and_past_deals(merchant_id).pluck(:title)
-      @active_deal_ids = MerchantService.get_all_active_and_past_deals(merchant_id).pluck(:id)
+      deal_analytics_by_type_and_redemption
     else
-      @active_deal_names = MerchantService.get_all_active_deals(merchant_id).pluck(:title)
-      @active_deal_ids = MerchantService.get_all_active_deals(merchant_id).pluck(:id)
-    end
-
-    # Get view counts and redemption counts for active deals
-    @view_counts = Array.new
-    @redemption_counts = Array.new
-    @active_deal_ids.each do |id|
-      @view_counts.push DealAnalyticService.get_view_count(id)
-      @redemption_counts.push DealAnalyticService.get_redemption_count(id)
+      render "analytics/error"
     end
   end
 
-  def deal_statistics_deal_type
-=begin
-    @merchant_deal_types = MerchantService.get_all_deals(merchant_id).pluck('DISTINCT type_of_deal')
-    @all_redemption = Array.new
-    @merchant_deal_types.each do |type|
-      deal_ids = DealService.get_all_ids_by_type_and_merchant(type, merchant_id)
-      redemption = DealAnalyticService.get_redemption_count(deal_ids)
-      @all_redemption.push redemption
+  def deal_statistics_by_venue
+    if check_has_deal_statistics
+      # Show popularity of venue
+      deal_analytics_by_venue
+      # Show popularity of deals in venue
+      deal_analytics_by_deals_for_venues
+    else
+      render "analytics/error"
     end
-    #raise @all_redemption.inspect
-=end
-    # Get deal type and redemption counts for chart
-    deal_ids = DealService.get_all_ids_by_type_and_merchant("Discount", merchant_id)
-    @redemption_discount = DealAnalyticService.get_redemption_count(deal_ids)
-    deal_ids = DealService.get_all_ids_by_type_and_merchant("Freebies", merchant_id)
-    @redemption_freebies = DealAnalyticService.get_redemption_count(deal_ids)
   end
 
+  private
   def deal_analytics_by_day
     @deals_daily_count = DealAnalyticService.get_analytics_for_line_graph(merchant_id, Date.today.beginning_of_quarter, Date.today)
   end
 
+  private
   def deal_analytics_by_type_and_redemption
     @deals_popularity_by_type_and_redemption = DealAnalyticService.get_analytics_for_deals_pie_chart(merchant_id)
   end
 
+  private
   def deal_analytics_by_venue
     @deals_by_venue = DealAnalyticService.get_analytics_for_deals_by_venue(merchant_id)
   end
 
+  private
   def deal_analytics_by_deals_for_venues
     @deals_for_venue = DealAnalyticService.get_analytics_for_venues_by_deals(merchant_id)
   end
