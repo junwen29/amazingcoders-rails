830d996fb6375b67cb700afdc698c5cf65624cc1
Jack <jack@shopback.com>
2015-10-20 21:54:35 +0800

    Check deal analytics only when merchant is signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,24 +26,25 @@ class ApplicationController < ActionController::Base
     head :not_found
   end
 
+  # @deal_analytics = 0: No access
+  # @deal_analytics = 1: Has deal statistics and deal aggregate trends
+  # @deal_analytics = 2: Has deal statistics only
+  # @deal_analytics = 3: Has deal aggregate only
   private
   def check_deal_analytics
-    payment = Payment.where("merchant_id = ? AND start_date <= ? AND expiry_date >= ? AND paid = ?", merchant_id, Date.today, Date.today, true).last
-    # @deal_analytics = 0: No access
-    # @deal_analytics = 1: Has deal statistics and deal aggregate trends
-    # @deal_analytics = 2: Has deal statistics
-    # @deal_analytics = 3: Has deal aggregate
-    if payment.blank?
-      @deal_analytics = 0
-    else
-      if payment.add_on2 && payment.add_on3
-        @deal_analytics = 1
-      elsif payment.add_on2 && !payment.add_on3
-        @deal_analytics = 2
-      elsif !payment.add_on2 && payment.add_on3
-        @deal_analytics = 3
+    @deal_analytics = 0
+    if merchant_signed_in?
+      payment = Payment.where("merchant_id = ? AND start_date <= ? AND expiry_date >= ? AND paid = ?", merchant_id, Date.today, Date.today, true).last
+      if payment.present?
+        if payment.add_on2 && payment.add_on3
+          @deal_analytics = 1
+        elsif payment.add_on2 && !payment.add_on3
+          @deal_analytics = 2
+        elsif !payment.add_on2 && payment.add_on3
+          @deal_analytics = 3
+        end
       end
     end
-  end
+    end
 
 end
