6ec9259cd3d18e77d9f9f7bb5ff29b323129d5f4
junwen29 <junwen2992@gmail.com>
2015-10-22 22:33:09 +0800

    update merchant test

--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class MerchantTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  #========================== test positive ========================
+  test "register merchant" do
+    assert User.create! :name => 'John Doe', :email => 'john@gmail.com',
+                        :password => 'topsecret', :password_confirmation => 'topsecret'
+  end
+
+  #========================== test negative ========================
+
+  test "should not save merchant if email is not unique" do
+    merchant_one = Merchant.new
+    merchant_one.email = 'test@example.com'
+    merchant_one.encrypted_password = '#$taawktljasktlw4aaglj'
+    assert merchant_one.save!
+
+    merchant_two = Merchant.new
+    merchant_one.email = 'test@example.com'
+    merchant_two.encrypted_password = '#$taawktljasktlw4aaglj'
+    assert_no merchant_two.save!
+  end
+
 end
