817249c4d54a90a44e01317c792787a4c818a57a
junwen29 <junwen2992@gmail.com>
2015-10-22 21:42:30 +0800

    add device test case

--- test/models/device_test.rb
+++ test/models/device_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class DeviceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should not save device without device type" do
+    device = Device.new
+    assert_no device.save
+  end
+
+  test "should not save device if token is not unique to user" do
+    device_one = Device.new(:user_id => "1", :token => "token", :device_type => "android_type")
+    assert device_one.save
+    device_two = Device.new(:user_id => "1", :token => "token", :device_type => "android_type")
+    assert_no device_two.save
+  end
+
 end
