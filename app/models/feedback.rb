10c174bf8e7475dd053286452e2aa48ffb6fb077
Derek Lau <dereklau92@gmail.com>
2015-10-27 07:13:18 +0800

    added form validation, implement admin side for merchant forms

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,5 +1,10 @@
 class Feedback < ActiveRecord::Base
 
   belongs_to :merchant
+
+  validates(:title, presence: true)
+  validates(:category, presence: true)
+  validates(:content,presence: true)
+
   
 end
