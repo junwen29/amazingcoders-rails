33a83967a55446a43ce9b4c78d25c72ef347800e
junwen29 <junwen2992@gmail.com>
2015-10-28 00:25:17 +0800

    change from hardcode ref to t.belongs to association

--- db/migrate/20151025165759_create_user_points.rb
+++ db/migrate/20151025165759_create_user_points.rb
@@ -1,10 +1,10 @@
 class CreateUserPoints < ActiveRecord::Migration
   def change
     create_table :user_points do |t|
-      t.string :reason
-      t.integer :points
-      t.string :operation
-      t.integer :user_id
+      t.string      :reason
+      t.integer     :points
+      t.string      :operation
+      t.belongs_to  :user, index: true
 
       t.timestamps
     end
