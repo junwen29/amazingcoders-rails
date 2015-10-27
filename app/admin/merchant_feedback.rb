ed7f0bae21dc162ea52069767773696c62d3aa28
Jack <jack@shopback.com>
2015-10-27 23:17:32 +0800

    hotfixes to FM and admin feedback

--- app/admin/merchant_feedback.rb
+++ app/admin/merchant_feedback.rb
@@ -1,24 +1,11 @@
 ActiveAdmin.register MerchantFeedback do
   config.clear_action_items!
 
-  menu :parent => "Merchant", :priority => 3
+  menu :parent => "Merchant", :priority => 4
 
   actions :index, :show, :edit
 
-  config.batch_actions = false
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
 
 
 end
