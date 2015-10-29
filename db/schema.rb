5a6693e7f14ab11b2a428ad61c5b9e659558a24e
Woonyong92 <woonyong92@gmail.com>
2015-10-29 19:12:36 +0800

    For popularity of Deals in Venues, changed wrong label "Venue" to "Redemptions"

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151027154533) do
+ActiveRecord::Schema.define(version: 20151029071136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -319,7 +319,7 @@ ActiveRecord::Schema.define(version: 20151027154533) do
     t.string   "authentication_token"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.integer  "total_points"
+    t.integer  "total_points",           default: 0
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
