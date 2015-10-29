acaabcf902387cc9940814781993c6e53f9ded6c
Jack <jack@shopback.com>
2015-10-29 11:29:18 +0800

    Added edit function to update status and category of user feedback from admin

--- app/admin/user_feedback.rb
+++ app/admin/user_feedback.rb
@@ -22,12 +22,27 @@ ActiveAdmin.register UserFeedback do
     column :content
     column :created_at
     column "Status", :resolved do |feedback|
-      feedback ? status_tag( "Resolved", :ok ) : status_tag( "Unresolved" )
+      feedback.resolved ? status_tag( "Resolved", :ok ) : status_tag( "Unresolved" )
     end
     actions
   end
 
+  # EDIT
+  form do |f|
+    f.semantic_errors
+    f.inputs "Follow up Feedback" do
+      f.input :id, label: 'Ticket ID', :input_html => { :disabled => true }
+      f.input :user, :input_html => { :disabled => true }
+      f.input :title, label: 'Title', :input_html => { :disabled => true }
+      f.input :category, label: 'Category', as: :select, collection: ["Suggestion", "Complaint", "Issue"]
+      f.input :content, label: 'Content', :input_html => { :disabled => true }
+      f.input :created_at, as: :datepicker, :input_html => { :disabled => true }
+      f.input :resolved, label: 'Mark as resolved', as: :radio
+    end
+    f.actions
+  end
+
   # To comment off
-  permit_params :category, :content, :title, :resolved, :user_id
+  permit_params :category, :resolved
 
 end
