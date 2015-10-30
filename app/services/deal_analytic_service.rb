a5c4b13a4c8bc2286dd18e8087bfd7fdcaa162c8
Woonyong92 <woonyong92@gmail.com>
2015-10-30 23:35:22 +0800

    Added conversion rate from wishlist to views

--- app/services/deal_analytic_service.rb
+++ app/services/deal_analytic_service.rb
@@ -424,6 +424,16 @@ class DealAnalyticService
       deal_analytics
     end
 
+    # returns conversion rate of num wishlisters to viewers in percentage
+    def get_wishlist_to_views(deal_id)
+      activate_date = Deal.find(deal_id).activate_date
+      num_wishlist = WishService.num_wishlist_deal(deal_id, activate_date).to_f
+      user_id = WishService.get_user_id(deal_id, activate_date)
+      num_views = Viewcount.where(user_id: user_id).uniq.where(:entry => 'merchant_push_notification').count.to_f
+      conversion = (num_views/num_wishlist)*100
+      conversion.round(2)
+    end
+
   end
 
   class << self
