41855f5c38806a1d5f1d2e1c2ad41ae3ddc1d1a0
Jack <jack@shopback.com>
2015-11-01 13:45:50 +0800

    Added test cases for venue

--- test/models/venue_test.rb
+++ test/models/venue_test.rb
@@ -78,4 +78,58 @@ class VenueTest < ActiveSupport::TestCase
     venue = venue_one(name, street, zipcode, neighbourhood, phone)
     assert_not venue.save
   end
+
+  test "edit venue" do
+    # create venue
+    merchant = merchant_one
+    merchant.save
+    name = "Chicken Up @ Tampines"
+    street = '2 Tampines Central 5'
+    zipcode = '529509'
+    neighbourhood = 'Tampines'
+    phone = '65880308'
+    venue = venue_one(name, street, zipcode, neighbourhood, phone)
+    venue.save
+
+    # edit venue
+    name = "Chicken Down"
+    venue.update(name: name)
+    assert_equal "Chicken Down", venue.name
+  end
+
+  test "delete venue" do
+    # create venue
+    merchant = merchant_one
+    merchant.save
+    name = "Chicken Up @ Tampines"
+    street = '2 Tampines Central 5'
+    zipcode = '529509'
+    neighbourhood = 'Tampines'
+    phone = '65880308'
+    venue = venue_one(name, street, zipcode, neighbourhood, phone)
+    venue.save
+
+    # delete venue
+    assert_difference('Venue.count', -1) do
+      venue.destroy
+    end
+  end
+
+  test "view venue" do
+    # create venue id 9999
+    merchant = merchant_one
+    merchant.save
+    name = "Chicken Up @ Tampines"
+    street = '2 Tampines Central 5'
+    zipcode = '529509'
+    neighbourhood = 'Tampines'
+    phone = '65880308'
+    venue = venue_one(name, street, zipcode, neighbourhood, phone)
+    venue.save
+
+    # check venue id 9999 exists
+    venue = Venue.find(9999)
+    assert_not_nil venue
+  end
+
 end
