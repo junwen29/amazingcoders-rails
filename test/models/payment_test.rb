a536b97996032650bdf068bbfc3560e591238dd3
Jack <jack@shopback.com>
2015-11-01 13:19:33 +0800

    Added test cases for payment

--- test/models/payment_test.rb
+++ test/models/payment_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
 
 class PaymentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "save payment" do
+    merchant = merchant_one
+    merchant.save
+    payment = payment_one(Date.today, 1)
+    assert payment.save
+  end
+
+  test "should not save without start date" do
+    merchant = merchant_one
+    merchant.save
+    payment = payment_one(nil, 1)
+    assert_not payment.save
+  end
+
+  test "should not save without months" do
+    merchant = merchant_one
+    merchant.save
+    payment = payment_one(Date.today, nil)
+    assert_not payment.save
+  end
+
+  test "should not save if start date has passed" do
+    merchant = merchant_one
+    merchant.save
+    payment = payment_one(Date.yesterday, 1)
+    assert_not payment.save
+  end
+
+  test "should not save if plans overlap" do
+    merchant = merchant_one
+    merchant.save
+    payment1 = payment_one(Date.today, 1)
+    payment1.save
+    payment2 = payment_one(Date.tomorrow, 1)
+    assert_not payment2.save
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -23,4 +23,12 @@ class ActiveSupport::TestCase
     merchant.password_confirmation = 'topsecret'
     merchant
   end
+
+  def payment_one(start_date, months)
+    end_date = Date.today + 1.month
+    cost  = 45 * 1
+    Payment.new(id: 9999, start_date: start_date, expiry_date: end_date,
+                total_cost: cost, add_on1: true, add_on2: true, add_on3: true, plan1: true,
+                paid: true, merchant_id: '9999', months: months)
+  end
 end
