135ff015aaa67b262c5b50b8f1ace3a1e29741ff
Jack <jkcheong92@gmail.com>
2015-11-03 22:37:47 +0800

    Allow admin to edit merchant points

--- app/admin/merchant.rb
+++ app/admin/merchant.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register Merchant do
   config.clear_action_items!
 
   menu :parent => "Merchant", :priority => 1
-  actions :all, except: [:edit]
+  actions :all
 
   filter :venues
   filter :deals
@@ -50,5 +50,17 @@ ActiveAdmin.register Merchant do
     active_admin_comments
   end
 
+  # EDIT
+  form do |f|
+    f.semantic_errors
+    f.inputs "Edit points" do
+      f.input :id, label: 'Merchant ID', :input_html => { :disabled => true }
+      f.input :email, :input_html => { :disabled => true }
+      f.input :total_points, label: 'Merchant Points'
+    end
+    f.actions
+  end
+
+  permit_params :total_points
 
 end
