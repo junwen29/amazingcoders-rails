527dc9ff2d1598c2cb887aaf284e6bcef04ef6f8
junwen29 <junwen2992@gmail.com>
2015-11-03 12:13:32 +0800

    migrate using device and notification service

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -145,29 +145,13 @@ class DealsController < ApplicationController
 
   # Change non-active deal to active
   def push
-    tokens = []
-    venues = DealService.get_all_venues(@deal.id)
-
-    # load tokens via users wishes
-    venues.each { |venue|
-      users = VenueService.wishes_by_venue(venue.id)
-      users.each { |user|
-        user.devices.each { |device|
-          tokens << device.token
-        }
-      }
-    }
-    gcm = GCM.new("AIzaSyBGQPh58s2ow6H_OabGrh4vRmzNaNkdRcU")
-
+    tokens = DeviceService.tokens_by_venue_wishlist(@deal.id)
     item_type = "deal"
     item_id = @deal.id
     item_name = @deal.title
     message = '**Check out the new deal now!** Click to view more details.'
 
-    options = {data:
-                   {message: message, item_type: item_type, item_id: item_id.to_s, item_name: item_name.to_s}
-    }
-    response = gcm.send(tokens, options)
+    NotificationService.send_notification(tokens, item_type,item_id, item_name, message)
 
     @deal.update_attribute(:pushed, true)
 
