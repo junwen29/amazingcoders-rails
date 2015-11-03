1c254d7472c2cba61a3dc19ecd49619625d890f8
junwen29 <junwen2992@gmail.com>
2015-11-04 00:50:23 +0800

    update date format in json parsing

--- app/models/notification/json.rb
+++ app/models/notification/json.rb
@@ -4,11 +4,12 @@ module Notification::Json
   included do
 
     def to_json(json, options = {})
+      json.id         self.id
       json.item_type  self.item_type
       json.item_id    self.item_id
       json.item_name  self.item_name
       json.message    self.message
-      json.created_at self.created_at
+      json.created_at self.created_at.strftime("%Y-%m-%dT%H:%M:%SZ%Z")
     end
 
   end
