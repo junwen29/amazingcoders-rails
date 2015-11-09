2a7fdce7333e2f18f0e78c2640644f3d86e11685
gsm99 <gohserming@gmail.com>
2015-11-10 00:38:11 +0800

    without paying, the payment would not be updated. upon paying, a new payment object is created while the old one is deleted. (hence there is a new payment id)

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -28,8 +28,8 @@ class ChargesController < ApplicationController
           :description => 'Rails Stripe customer',
           :currency    => 'sgd'
       )
-        # Send out payment acknowledgement email
-        PaymentMailer.subscription_email("valued merchant", @payment, MerchantService.get_email(merchant_id)).deliver
+      # Send out payment acknowledgement email
+      PaymentMailer.subscription_email("valued merchant", @payment, MerchantService.get_email(merchant_id)).deliver
 
     rescue Stripe::CardError => e
       flash[:error] = e.message
@@ -56,9 +56,28 @@ class ChargesController < ApplicationController
       MerchantPointService.new_point("12 months milestone reward", 500, "Credit", merchant_id)
       flash[:success] = "Plan upgrade completed! You have been awarded " + @payment.total_cost.to_i.to_s + " Burps! Thanks for being a loyal user! You have been credited an extra 500 Burps!"
     end
+    old_payment = Payment.where(:merchant_id => merchant_id, :start_date => @payment.start_date)
+    old_payment.each do |p|
+      if p && (p.id != @payment.id)
+        PaymentService.destroy(p)
+      end
+    end
 
     @merchant.update(total_points: total_points)
     redirect_to payments_path
 
+  end
+
+
+  def new_modify
+    @payment = Payment.find(params[:payment_id])
+
+    unless session[:merchant_id] == @payment.merchant_id #&& @payment.paid == false
+      flash[:error] = "You don't have access to this page!"
+      redirect_to payments_path
+      return
+    end
+    #total_cost = @payment.total_cost
+    #@payment.update(expiry_date: @payment.start_date.months_since(@payment.months))
   end
 end
